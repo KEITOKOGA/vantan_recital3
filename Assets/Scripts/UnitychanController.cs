0437f6bfdcfc5b78d1ef039a4b9c1f205e56657b
KEITOKOGA <134474068+KEITOKOGA@users.noreply.github.com>
2023-08-04 18:02:13 +0900

    カメラがプレイヤーを追従するスクリプトと左クリックした際に攻撃するスクリプトを書いた

--- Assets/Scripts/UnitychanController.cs
+++ Assets/Scripts/UnitychanController.cs
@@ -8,7 +8,7 @@ public class UnitychanController : MonoBehaviour
     [SerializeField] float _moveSpeed = 1.0f;   //ユニティーちゃんの移動速度
     [SerializeField] float _jumpPower = 3.0f;   //ユニティーちゃんのジャンプの強さ
     [SerializeField] float _dashSpeed = 2.0f;   //shiftを押した際の加速度
-    [SerializeField] float _attackInterval = 1.0f;    //攻撃のクールタイム
+    [SerializeField] float _attackInterval;     //攻撃のクールタイム
     [SerializeField] float _gravityDrag = .8f; //ジャンプボタンを離した際の落下速度の上昇率
     Rigidbody2D _rb = default;
     bool _isGrounded = false;
@@ -30,9 +30,10 @@ public class UnitychanController : MonoBehaviour
         _attackTimer += Time.deltaTime;
         _h = Input.GetAxisRaw("Horizontal");
         Vector2 velocity = _rb.velocity;
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && _attackTimer > _attackInterval)
         {
-
+            _attackTimer = 0;
+            _anim.Play("Unitychan_Sword_Combo1", 0);
         }
         if (Input.GetButtonDown("Jump") && _isGrounded)
         {
@@ -53,12 +54,10 @@ public class UnitychanController : MonoBehaviour
         if (!Input.GetButton("Fire3"))
         {
             _rb.AddForce(Vector2.right * _h * _moveSpeed);
-            Debug.Log("通常速度で動作");
         }
         else
         {
             _rb.AddForce(Vector2.right * _h * _moveSpeed * _dashSpeed);
-            Debug.Log("ダッシュで動作");
         }
     }
     void OnTriggerEnter2D(Collider2D collision)
